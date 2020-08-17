5f83263eae95aa65b5ed83258e7ae90caeaf589f
mathew98 <43654709+matanlaviv98@users.noreply.github.com>
2020-08-17 20:09:35 +0300

    90 on testing

--- project.py
+++ project.py
@@ -11,6 +11,7 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 train_dir = r'chest_xray\train'
 validation_dir = r'chest_xray\val'
+test_dir = r'chest_xray\test'
 
 BATCH_SIZE = 32
 IMG_SIZE = (160, 160)
@@ -20,6 +21,10 @@ train_dataset = image_dataset_from_directory(train_dir,
                                              batch_size=BATCH_SIZE,
                                              image_size=IMG_SIZE)
 
+test_dataset = image_dataset_from_directory(test_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
 
 validation_dataset = image_dataset_from_directory(validation_dir,
                                                   shuffle=True,
@@ -28,17 +33,6 @@ validation_dataset = image_dataset_from_directory(validation_dir,
 
 class_names = train_dataset.class_names
 
-
-    
-val_batches = tf.data.experimental.cardinality(validation_dataset)
-test_dataset = validation_dataset.take(val_batches // 5)
-validation_dataset = validation_dataset.skip(val_batches // 5)
-
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
-validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
-test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
 data_augmentation = tf.keras.Sequential([
   tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
@@ -52,7 +46,10 @@ base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
                                                include_top=False,
                                                weights='imagenet')
 
-base_model.trainable = False
+fine_tune_at = 100
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable =  False
+
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 prediction_layer = tf.keras.layers.Dense(1)
 
@@ -71,12 +68,11 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-initial_epochs = 30
+initial_epochs = 10
 
 loss0, accuracy0 = model.evaluate(validation_dataset)
 
 history = model.fit(train_dataset,
-                    epochs=initial_epochs,
                     validation_data=validation_dataset)
 
 
@@ -86,21 +82,27 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.ylabel('Accuracy')
-plt.ylim([min(plt.ylim()),1])
-plt.title('Training and Validation Accuracy')
+# =============================================================================
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([min(plt.ylim()),1])
+# plt.title('Training and Validation Accuracy')
+# 
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.ylabel('Cross Entropy')
+# plt.ylim([0,1.0])
+# plt.title('Training and Validation Loss')
+# plt.xlabel('epoch')
+# plt.show()
+# =============================================================================
 
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.ylabel('Cross Entropy')
-plt.ylim([0,1.0])
-plt.title('Training and Validation Loss')
-plt.xlabel('epoch')
-plt.show()
+score = model.evaluate(test_dataset)
+
+print (score)
