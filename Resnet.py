e5df4fab238c83325487f1403260802813b7fe60
mathew98 <43654709+matanlaviv98@users.noreply.github.com>
2020-08-18 19:48:10 +0300

    changing back to resnet

---
+++ Resnet.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,MaxPool2D,Dropout,Flatten, MaxPooling2D, Input, GlobalAveragePooling2D
+from keras.optimizers import RMSprop,Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.callbacks import EarlyStopping
+
+test_dir = r'chest_xray\test'
+train_dir = r'chest_xray\train'
+val_dir = r'chest_xray\val'
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+train_dataset = image_dataset_from_directory(train_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+
+test_dataset = image_dataset_from_directory(test_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+
+val_dataset = image_dataset_from_directory(val_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+
+
+class_names = train_dataset.class_names
+
+
+
+preprocess = tf.keras.applications.resnet.preprocess_input
+
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+img_shape = (160,160,3)
+base_model = tf.keras.applications.ResNet50(weights='imagenet',input_shape = img_shape, include_top = False)
+#base_model.trainable = False #freeze all, add fine tuning later
+
+for layer in base_model.layers:
+    if isinstance(layer, tf.keras.layers.BatchNormalization):
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+
+#rescale = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+
+l2 = tf.keras.layers.Dense(256, activation = 'relu')
+prediction_layer = tf.keras.layers.Dense(1)
+
+
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x=preprocess(inputs)
+x = data_augmentation(x)
+
+x = base_model(x)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.25)(x)
+#x= tf.keras.layers.BatchNormalization(x)
+x = l2(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#base_model.summary()
+
+initial_epochs = 30
+es = EarlyStopping(monitor='val_accuracy' , mode='max', patience = 5, restore_best_weights=True)
+loss0, accuracy0 = model.evaluate(val_dataset)
+
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+
+history = model.fit(train_dataset,
+                    epochs=initial_epochs,
+                    validation_data=val_dataset,
+                    callbacks=[es])
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+score = model.evaluate(test_dataset)
+print(score)
--- project.py
+++ project.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import tensorflow as tf
-
+from tensorflow.keras.callbacks import EarlyStopping
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.applications.resnet50 import ResNet50 , preprocess_input
+from keras.models import Input , Model
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
@@ -14,7 +18,7 @@ validation_dir = r'chest_xray\val'
 test_dir = r'chest_xray\test'
 
 BATCH_SIZE = 32
-IMG_SIZE = (160, 160)
+IMG_SIZE = (100, 100)
 
 train_dataset = image_dataset_from_directory(train_dir,
                                              shuffle=True,
@@ -31,49 +35,53 @@ validation_dataset = image_dataset_from_directory(validation_dir,
                                                   batch_size=BATCH_SIZE,
                                                   image_size=IMG_SIZE)
 
-class_names = train_dataset.class_names
+# =============================================================================
+# class_names = train_dataset.class_names
+# 
+# 
+# data_augmentation = tf.keras.Sequential([
+#   tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+#   tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+# ])
+# =============================================================================
+
+input_shape = (100,100,3)
+base_model = ResNet50(weights='imagenet',input_shape = input_shape, include_top = False,
+                      pooling = 'average')
+base_model.trainable= False
+
+# =============================================================================
+# fine_tune_at = 100
+# for layer in base_model.layers[:fine_tune_at]:
+#     layer.trainable =  False
+# =============================================================================
+
+inputs = Input(shape=(100,100, 3))
+x = preprocess_input(inputs)
+x = base_model.output(x)
+x = GlobalAveragePooling2D()(x)
+x = Dense(1000, activation = 'relu')(x)
+x = Dense(1, activation = 'sigmoid')(x)
+model = Model(base_model.input,x)
 
 
-data_augmentation = tf.keras.Sequential([
-  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
-  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-])
-
-preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
-
-IMG_SHAPE = IMG_SIZE + (3,)
-base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
-
-fine_tune_at = 100
-for layer in base_model.layers[:fine_tune_at]:
-    layer.trainable =  False
-
-global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-prediction_layer = tf.keras.layers.Dense(1)
+initial_epochs = 40
 
 
-inputs = tf.keras.Input(shape=(160, 160, 3))
-x = data_augmentation(inputs)
-x = preprocess_input(x)
-x = base_model(x, training=False)
-x = global_average_layer(x)
-x = tf.keras.layers.Dropout(0.2)(x)
-outputs = prediction_layer(x)
-model = tf.keras.Model(inputs, outputs)
-
-base_learning_rate = 0.0001
-model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+model.compile(Adam(),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-initial_epochs = 10
+
+es = EarlyStopping(monitor='accuracy' , mode='max', patience = 1)
+
 
 loss0, accuracy0 = model.evaluate(validation_dataset)
 
 history = model.fit(train_dataset,
-                    validation_data=validation_dataset)
+                    epochs=initial_epochs,
+                    validation_data=validation_dataset,
+                    callbacks=[es])
 
 
 acc = history.history['accuracy']
@@ -82,26 +90,24 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-# =============================================================================
-# plt.figure(figsize=(8, 8))
-# plt.subplot(2, 1, 1)
-# plt.plot(acc, label='Training Accuracy')
-# plt.plot(val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.ylabel('Accuracy')
-# plt.ylim([min(plt.ylim()),1])
-# plt.title('Training and Validation Accuracy')
-# 
-# plt.subplot(2, 1, 2)
-# plt.plot(loss, label='Training Loss')
-# plt.plot(val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.ylabel('Cross Entropy')
-# plt.ylim([0,1.0])
-# plt.title('Training and Validation Loss')
-# plt.xlabel('epoch')
-# plt.show()
-# =============================================================================
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
 
 score = model.evaluate(test_dataset)
 
