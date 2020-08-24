ab598b76048b1ea2d059bf4b1e077113ee5df84d
StasRez <50587750+StasRez@users.noreply.github.com>
2020-08-24 13:21:23 +0300

    finished Q2_1_a

--- Resnet_Q2_1_a.py
+++ Resnet_Q2_1_a.py
@@ -2,10 +2,20 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+from google.colab import drive
 
-test_dir = r'chest_xray\test'
-train_dir = r'chest_xray\train'
-val_dir = r'chest_xray\val'
+
+in_colab = True
+if(in_colab):
+  drive.mount('/content/gdrive', force_remount=True)
+  root_dir = "/content/gdrive/My Drive/DL_DATA/chest_xray"
+  test_dir = root_dir + "/test"
+  train_dir = root_dir + "/train"
+  val_dir = root_dir + "/val"
+else:
+  test_dir = r'chest_xray\test'
+  train_dir = r'chest_xray\train'
+  val_dir = r'chest_xray\val' 
 
 BATCH_SIZE = 32
 IMG_SIZE = (160, 160)
@@ -41,6 +51,7 @@ img_shape = IMG_SIZE + (3,)
 base_model = tf.keras.applications.ResNet50(weights='imagenet',pooling = 'average',input_shape = img_shape, include_top = False)
 #base_model.trainable = False #freeze all, add fine tuning later
 
+#train only the BN layers
 for layer in base_model.layers:
     if isinstance(layer, tf.keras.layers.BatchNormalization):
         layer.trainable = True
@@ -51,7 +62,8 @@ for layer in base_model.layers:
 #rescale = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 
-dense_layer = tf.keras.layers.Dense(256, activation = 'relu')
+dense_layer = tf.keras.layers.Dense(1024, activation = 'relu')
+drop_out = tf.keras.layers.Dropout(0.35)
 prediction_layer = tf.keras.layers.Dense(1)
 
 
@@ -60,8 +72,7 @@ x=preprocess(inputs)
 x = data_augmentation(x)
 x = base_model(x)
 x = global_average_layer(x)
-#x = tf.keras.layers.Dropout(0.25)(x)
-#x= tf.keras.layers.BatchNormalization(x)
+x = drop_out(x)
 x = dense_layer(x)
 outputs = prediction_layer(x)
 model = tf.keras.Model(inputs, outputs)
@@ -73,16 +84,15 @@ model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
 
 model.summary()
 
-initial_epochs = 100
-es = EarlyStopping(monitor='val_loss' , mode='min', patience = 4, restore_best_weights=True)
+epochs = 14
+
 loss0, accuracy0 = model.evaluate(val_dataset)
 
 print("initial loss: {:.2f}".format(loss0))
 print("initial accuracy: {:.2f}".format(accuracy0))
 history = model.fit(train_dataset,
-                    epochs=initial_epochs,
-                    validation_data=val_dataset,
-                    callbacks = [es])
+                    epochs=epochs,
+                    validation_data=val_dataset)
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
@@ -90,7 +100,7 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-"""
+
 plt.figure(figsize=(8, 8))
 plt.subplot(2, 1, 1)
 plt.plot(acc, label='Training Accuracy')
@@ -109,7 +119,60 @@ plt.ylim([0,1.0])
 plt.title('Training and Validation Loss')
 plt.xlabel('epoch')
 plt.show()
-"""
+
+
 score = model.evaluate(test_dataset)
-model.save('Q2_1_a_Model')
+
+print(score)
+
+
+for idx in range(90):
+    base_model.layers[-idx].trainable=True
+
+base_learning_rate = 0.00001
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs = 14
+
+loss0, accuracy0 = model.evaluate(val_dataset)
+
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+history = model.fit(train_dataset,
+                    epochs=epochs,
+                    validation_data=val_dataset)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+
+score = model.evaluate(test_dataset)
+
 print(score)
